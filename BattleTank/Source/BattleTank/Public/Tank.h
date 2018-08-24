ea2d2600f0a802675a61c8f14f1d6a4573490afe
office\crossmr <crossmr@gmail.com>
2018-08-24 17:01:01 +0900

    140 SuggestProjectileVelocity

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -28,6 +28,9 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere, Category = "Firing")
+		float LaunchSpeed = 100000; //TODO Find sensible default
+
 
 protected:
 	// Called when the game starts or when spawned
